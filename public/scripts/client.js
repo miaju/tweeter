0151c41dad411f2b34c25db08e6c3172a35c626d
Mia Tanner <mia@indygecko.com>
2022-10-10 14:53:35 -0700

    fetch tweets from database and display

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -40,34 +40,14 @@ $(document).ready(function() {
       $('#tweets-container').append(createTweetElement(tweet));
     }
   };
-  
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
-  
-  renderTweets(data);
 
+  const loadTweets = function() {
+    $.get("/tweets", function(data) {
+      console.log(data);
+      renderTweets(data);
+    });
+  };
+  
   const $newTweet = $('.new-tweet');
 
   $newTweet.submit(function(event) {
@@ -75,6 +55,8 @@ $(document).ready(function() {
     const serialized = $(event.target).serialize();
     $.post('/tweets', serialized);
   });
+
+  loadTweets();
   
 });
 
